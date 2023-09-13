f0a65e6d9d82b30cfa133c545bc3ef6b4fb94305
Benek Cichocki <bernard.cichocki99@gmail.com>
2023-09-13 18:28:41 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿ // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1 = 5;
+int number2 = 10;
+
+// == - równe
+// != - różne
+// < - większa liczba
+// > - mniejsza liczba
+// <= - większa lub równa liczba
+// >= - mniejsza lub równa liczba
+
+var name = "Bernard";
+var age = 22;
+
+if (!(age < 50))
+{
+    Console.WriteLine("Jestem Bernardem lub mam mniej niż 50");
+}
+else
+{
+    Console.WriteLine("Jestem kimś innym");
+}
+
+
